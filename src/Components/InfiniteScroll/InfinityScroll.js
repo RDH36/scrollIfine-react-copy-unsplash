dba48d63f83ed3f4df1de08dad2234598ce75747
Raymond <r.dzeryhago@bubble-go.mg>
2022-07-19 16:56:18 +0300

    fix: change attribute class to classname

--- src/Components/InfiniteScroll/InfinityScroll.js
+++ src/Components/InfiniteScroll/InfinityScroll.js
@@ -124,9 +124,9 @@ export default function InfinityScroll() {
           </div>
         </div>
       )}
-      <div class="d-flex justify-content-center mt-5">
-        <div class="spinner-border" role="status">
-          <span class="visually-hidden">Loading...</span>
+      <div className="d-flex justify-content-center mt-5">
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
         </div>
       </div>
     </div>
